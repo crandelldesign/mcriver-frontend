cc7dc81a4956bb6324661caa237376af3de0339e
Matt Crandell <mrcrandell@gmail.com>
2018-05-12 14:48:42 -0400

    Added login status to checkout. Fixed stripes.

--- src/app/pages/place-order/place-order.component.ts
+++ src/app/pages/place-order/place-order.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 import { Item } from '../../product/item';
 import { ProductService } from '../../product/product.service';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'mc-place-order',
@@ -10,10 +12,13 @@ import { ProductService } from '../../product/product.service';
 export class PlaceOrderComponent implements OnInit {
 
   items: Item[];
-  isFirstOpen = true;
+  isOpen = 1;
+
+  loggedInSub: Subscription;
 
   constructor(
-    public productsService: ProductService
+    public productsService: ProductService,
+    private userService: UserService
   ) { 
     this.productsService.people = [];
   }
@@ -21,10 +26,8 @@ export class PlaceOrderComponent implements OnInit {
   ngOnInit() {
     //this.productsService.fetchCategories();
     this.productsService.fetchCart();
-    console.log(this.productsService.cartItems);
     this.productsService.cartItems.forEach((itemObject) =>{
       let item = itemObject['item'];
-      console.log(item);
       if (item.slug == 'camping-people-in-group') {
         for (let index = 0; index < item.quantity; index++) {
           this.productsService.people.push({
@@ -36,6 +39,21 @@ export class PlaceOrderComponent implements OnInit {
       }
     });
     console.log(this.productsService.people);
+    this.loggedInSub = this.userService.loggedIn$.subscribe(
+      loggedIn => {
+        console.log('hi');
+        if (this.userService.user.loggedIn) {
+          this.isOpen = 2;
+          if (this.productsService.people[0]!=undefined) {
+            this.productsService.people[0].name = this.userService.user.name;
+          }
+        }
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.loggedInSub.unsubscribe();
   }
 
 }
