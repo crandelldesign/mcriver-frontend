c168daff5d57fdf17593a4a716a09bcec8c0faaf
Matt Crandell <mrcrandell@gmail.com>
2018-05-13 22:52:30 -0400

    Added accordian functionality. Began order submitting.

--- src/app/pages/place-order/place-order.component.ts
+++ src/app/pages/place-order/place-order.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { Item } from '../../product/item';
 import { ProductService } from '../../product/product.service';
 import { UserService } from '../../user/user.service';
+
+import { CollapseDirective } from 'ngx-bootstrap/collapse';
+import { Order } from '../../order/order';
 
 @Component({
   selector: 'mc-place-order',
@@ -11,8 +14,17 @@ import { UserService } from '../../user/user.service';
 })
 export class PlaceOrderComponent implements OnInit {
 
+  order = new Order();
   items: Item[];
-  isOpen = 1;
+  isPanel1Open = false;
+  isPanel2Open = false;
+  isPanel3Open = false;
+  isPanel4Open = false;
+  @ViewChild('collapse1') collapse1: CollapseDirective;
+  @ViewChild('collapse2') collapse2: CollapseDirective;
+  @ViewChild('collapse3') collapse3: CollapseDirective;
+  @ViewChild('collapse4') collapse4: CollapseDirective;
+  collapseFirstLoad = false;
 
   loggedInSub: Subscription;
 
@@ -24,7 +36,7 @@ export class PlaceOrderComponent implements OnInit {
   }
 
   ngOnInit() {
-    //this.productsService.fetchCategories();
+    //
     this.productsService.fetchCart();
     this.productsService.cartItems.forEach((itemObject) =>{
       let item = itemObject['item'];
@@ -38,22 +50,36 @@ export class PlaceOrderComponent implements OnInit {
         }
       }
     });
-    console.log(this.productsService.people);
     this.loggedInSub = this.userService.loggedIn$.subscribe(
       loggedIn => {
-        console.log('hi');
         if (this.userService.user.loggedIn) {
-          this.isOpen = 2;
+          //this.isPanel1Open = false;
+          //this.isPanel2Open = true;
+          this.collapse2.show();
           if (this.productsService.people[0]!=undefined) {
             this.productsService.people[0].name = this.userService.user.name;
           }
+          this.order.email = this.userService.user.email;
         }
       }
-    )
+    );
   }
 
   ngOnDestroy() {
     this.loggedInSub.unsubscribe();
+  }
+
+  onAccordionChange(number,event) {
+    // Runs through all numbers on first load
+    console.log(number);
+    (number != 1)?this.collapse1.hide():'';
+    (number != 2)?this.collapse2.hide():'';
+    (number != 3)?this.collapse3.hide():'';
+    (number != 4)?this.collapse4.hide():'';
+    if (!this.collapseFirstLoad && number == 4) {
+      this.collapseFirstLoad = true;
+      this.collapse1.show();
+    }
   }
 
 }
